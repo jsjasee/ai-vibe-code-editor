9281fec7401a237f62e5802eefdcb0309f1c0854
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-04-06 13:30:42 +0800

    fix issue with prisma client not working

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 // this code is to fix the hot-reload problem that nextjs has with prisma client. ensure only 1 instance of prisma.
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client"; // since i generated the prisma client and i have a output file, make sure to import the client from here, not @prisma/client
 
 const prismaClientSingleTon = () => {
   return new PrismaClient(); // this gives us a new instance of prisma
