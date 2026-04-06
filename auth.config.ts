7e2377fb0a324ba4adcdb939f23716a518382fe7
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-04-06 13:59:08 +0800

    add basic configuration for auth.js

---
+++ auth.config.ts
@@ -0,0 +1,18 @@
+// we are using 2 providers here
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import type { NextAuthConfig } from "next-auth";
+
+export default {
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+} satisfies NextAuthConfig;
+// what's this satisfies keyword??
