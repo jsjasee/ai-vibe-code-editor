91fdac45f15cb609bb57fa694acd2bc5be7f1aac
Just Jase <tayjiasheng@Jias-MacBook-Pro.local>
2026-04-07 11:41:52 +0800

    implemented sign in page

---
+++ app/(auth)/auth/sign-in/page.tsx
@@ -0,0 +1,21 @@
+// we cannot use "use client" here because the SignInFormClient uses the server components?
+import SignInFormClient from "@/modules/auth/components/sign-in-form-client";
+import Image from "next/image";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <Image
+        src={"/login.svg"}
+        alt="Login-Image"
+        height={300}
+        width={300}
+        className="m-6 object-cover"
+      />
+      <SignInFormClient />
+    </>
+  );
+};
+
+export default Page;
